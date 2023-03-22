e8d2ea967f36fb4d309c18c47f7ca422459c39e8
Anastasia <anastasia.krivoruchko.work@gmail.com>
2023-03-22 18:17:51 +0200

    create function

---
+++ script.js
@@ -0,0 +1,16 @@
+function main (a = 2, b = 3, c) {
+    if(typeof(c) === "function") 
+        return c(sum(a, b));
+    else
+        return sum(a, b);
+}
+
+function sum (a = 2, b = 3) { 
+    return a + b; 
+}
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+console.log(main(10, 20, isEven));
